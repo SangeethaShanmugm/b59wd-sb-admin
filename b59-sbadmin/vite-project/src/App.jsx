ad097e59129ff1167ee453cd3f5e66350c2669d7
SangeethaShanmugm <sanjuriya22@gmail.com>
2024-06-05 20:58:14 +0530

    updated

---
+++ b59-sbadmin/vite-project/src/App.jsx
@@ -0,0 +1,35 @@
+import './App.css'
+import Sidebar from "./components/Sidebar"
+import Topbar from './components/Topbar'
+import Dashboard from './components/Dashboard';
+import {
+  BrowserRouter,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+function App() {
+
+  return (
+    <BrowserRouter>
+      <div id="wrapper">
+        <Sidebar />
+        <div id="content-wrapper" className="d-flex flex-column">
+          <div id="content">
+            <Topbar />
+            <div className="container-fluid">
+              <Switch>
+                <Route exact={true} path="/" component={Dashboard} />
+              </Switch>
+            </div>
+
+          </div>
+        </div>
+      </div>
+
+    </BrowserRouter>
+  )
+}
+
+export default App
